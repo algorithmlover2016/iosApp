996973ea8a8c58cc3023a222a2077c3b5fc1aba0
algorithmlover2016 <yubo.upt@gmail.com>
2023-07-04 22:59:12 +0800

    add first version 1.0 for display video with url

--- VideoPlayerReconstruct/VideoPlayerReconstruct/Controller/ViewController.swift
+++ VideoPlayerReconstruct/VideoPlayerReconstruct/Controller/ViewController.swift
@@ -9,29 +9,32 @@ import UIKit
 import AVFoundation
 import AVKit
 
+/*
 class ViewController: UIViewController {
 
     var player: AVPlayer?
     var playerViewController: AVPlayerViewController?
-    
+
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var progressSlider: UISlider!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let videoURL = URL(string: "https://joy1.videvo.net/videvo_files/video/free/2019-05/large_watermarked/190516_06_AZ-LAGOA-30_preview.mp4")
         // https://joy.videvo.net/videvo_files/video/premium/partners0524/large_watermarked/BB_01bbb1fd-672a-4b8c-81fb-34a99ced87d4_preview.mp4
         player = AVPlayer(url: videoURL!)
-        
+
         playerViewController = AVPlayerViewController()
         playerViewController?.player = player
-        
+        playerViewController?.delegate = self
+        playerViewController?.modalPresentationStyle = .fullScreen
+
         addChild(playerViewController!)
         view.addSubview(playerViewController!.view)
         playerViewController!.view.frame = view.bounds
-        
+
         // Observe player's current time
         player?.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC)), queue: DispatchQueue.main) { [weak self] time in
             guard let duration = self?.player?.currentItem?.duration else { return }
@@ -40,18 +43,29 @@ class ViewController: UIViewController {
             let progress = Float(currentSeconds / totalSeconds)
             self?.progressSlider.value = progress
         }
+
+        // Present the player view controller full screen
+        // presentPlayerFullScreen()
     }
-    
+
+
+    func presentPlayerFullScreen() {
+        guard let playerViewController = playerViewController else { return }
+
+        present(playerViewController, animated: true) {
+            self.player?.play()
+        }
+    }
     // Play button action
     @IBAction func playButtonPressed(_ sender: UIButton) {
         player?.play()
     }
-    
+
     // Pause button action
     @IBAction func pauseButtonPressed(_ sender: UIButton) {
         player?.pause()
     }
-    
+
     // Slider value changed action
     @IBAction func progressSliderValueChanged(_ sender: UISlider) {
         let duration = player?.currentItem?.duration.seconds ?? 0
@@ -60,3 +74,53 @@ class ViewController: UIViewController {
         player?.seek(to: time)
     }
 }
+
+
+extension ViewController: AVPlayerViewControllerDelegate {
+    func playerViewControllerDidStopPictureInPicture(_ playerViewController: AVPlayerViewController) {
+        // Dismiss the full screen player when Picture in Picture mode ends
+        playerViewController.dismiss(animated: true, completion: nil)
+    }
+}
+ */
+
+
+class ViewController: UIViewController {
+
+    var player: AVPlayer?
+    var playerViewController: AVPlayerViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let videoURL = URL(string: "https://joy.videvo.net/videvo_files/video/premium/partners0524/large_watermarked/BB_01bbb1fd-672a-4b8c-81fb-34a99ced87d4_preview.mp4")
+        player = AVPlayer(url: videoURL!)
+
+        playerViewController = AVPlayerViewController()
+        playerViewController?.player = player
+        playerViewController?.delegate = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        presentPlayerFullScreen()
+    }
+
+    func presentPlayerFullScreen() {
+        guard let playerViewController = playerViewController else { return }
+
+        playerViewController.modalPresentationStyle = .fullScreen
+        present(playerViewController, animated: true) {
+            self.player?.play()
+        }
+    }
+}
+
+extension ViewController: AVPlayerViewControllerDelegate {
+    func playerViewControllerDidStopPictureInPicture(_ playerViewController: AVPlayerViewController) {
+        // Dismiss the full screen player when Picture in Picture mode ends
+        playerViewController.dismiss(animated: true, completion: nil)
+    }
+}
+
