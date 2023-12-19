4df14ededc7df8556f473b6a7262afb3b9a02072
algorithmlover2016 <yubo.upt@gmail.com>
2023-12-19 11:34:51 +0800

    add text, micphone and camera version

---
+++ V2Fault/V2Fault/V2FaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  V2FaultApp.swift
+//  V2Fault
+//
+//  Created by bo yu on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct V2FaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
